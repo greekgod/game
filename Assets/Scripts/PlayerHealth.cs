f100380003dae23e76e9541c79ee398057d23d52
Gabriel Lett <gm.lett@gmail.com>
2014-09-04 20:18:33 -0300

    Added enemy death to botton colision with enemy

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -14,6 +14,7 @@ public class PlayerHealth : MonoBehaviour
 	private Vector3 healthScale;				// The local scale of the health bar initially (with full health).
 	private PlayerControl playerControl;		// Reference to the PlayerControl script.
 	private Animator anim;						// Reference to the Animator on the player
+	private Score score;
 
 
 	void Awake ()
@@ -25,6 +26,7 @@ public class PlayerHealth : MonoBehaviour
 
 		// Getting the intial scale of the healthbar (whilst the player has full health).
 		healthScale = healthBar.transform.localScale;
+		score = GameObject.Find("Score").GetComponent<Score>();	
 	}
 
 
@@ -33,41 +35,55 @@ public class PlayerHealth : MonoBehaviour
 		// If the colliding gameobject is an Enemy...
 		if(col.gameObject.tag == "Enemy")
 		{
-			// ... and if the time exceeds the time of the last hit plus the time between hits...
-			if (Time.time > lastHitTime + repeatDamagePeriod) 
-			{
-				// ... and if the player still has health...
-				if(health > 0f)
+			ContactPoint2D contact = col.contacts[0];
+			if(Vector3.Dot(contact.normal, Vector3.up) > 0.5){
+				// ... find the Enemy script and call the Hurt function.
+				col.gameObject.GetComponent<Enemy>().Hurt();
+								
+				// Destroy the rocket.
+				Destroy(col.gameObject);
+
+				// Increase the score by 100 points
+				score.score += 100;
+
+			}else{
+
+				// ... and if the time exceeds the time of the last hit plus the time between hits...
+				if (Time.time > lastHitTime + repeatDamagePeriod) 
 				{
-					// ... take damage and reset the lastHitTime.
-					TakeDamage(col.transform); 
-					lastHitTime = Time.time; 
-				}
-				// If the player doesn't have health, do some stuff, let him fall into the river to reload the level.
-				else
-				{
-					// Find all of the colliders on the gameobject and set them all to be triggers.
-					Collider2D[] cols = GetComponents<Collider2D>();
-					foreach(Collider2D c in cols)
+					// ... and if the player still has health...
+					if(health > 0f)
 					{
-						c.isTrigger = true;
+						// ... take damage and reset the lastHitTime.
+						TakeDamage(col.transform); 
+						lastHitTime = Time.time; 
 					}
-
-					// Move all sprite parts of the player to the front
-					SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer>();
-					foreach(SpriteRenderer s in spr)
+					// If the player doesn't have health, do some stuff, let him fall into the river to reload the level.
+					else
 					{
-						s.sortingLayerName = "UI";
+						// Find all of the colliders on the gameobject and set them all to be triggers.
+						Collider2D[] cols = GetComponents<Collider2D>();
+						foreach(Collider2D c in cols)
+						{
+							c.isTrigger = true;
+						}
+
+						// Move all sprite parts of the player to the front
+						SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer>();
+						foreach(SpriteRenderer s in spr)
+						{
+							s.sortingLayerName = "UI";
+						}
+
+						// ... disable user Player Control script
+						GetComponent<PlayerControl>().enabled = false;
+
+						// ... disable the Gun script to stop a dead guy shooting a nonexistant bazooka
+						GetComponentInChildren<Gun>().enabled = false;
+
+						// ... Trigger the 'Die' animation state
+						anim.SetTrigger("Die");
 					}
-
-					// ... disable user Player Control script
-					GetComponent<PlayerControl>().enabled = false;
-
-					// ... disable the Gun script to stop a dead guy shooting a nonexistant bazooka
-					GetComponentInChildren<Gun>().enabled = false;
-
-					// ... Trigger the 'Die' animation state
-					anim.SetTrigger("Die");
 				}
 			}
 		}
