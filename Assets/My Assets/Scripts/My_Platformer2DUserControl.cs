d938deb17472504876dc5a4aa3688a20a9752713
Gabriel Tamassia Martinez <gabriel.tamassia.martinez@gmail.com>
2014-09-06 15:59:26 -0300

    Enemies walk from side to side, player healthbar is working partially

---
+++ Assets/My Assets/Scripts/My_Platformer2DUserControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(My_PlatformerCharacter2D))]
+public class My_Platformer2DUserControl : MonoBehaviour 
+{
+	private My_PlatformerCharacter2D character;
+    public bool jump;
+
+
+	void Awake()
+	{
+		character = GetComponent<My_PlatformerCharacter2D>();
+	}
+
+    void Update ()
+    {
+        // Read the jump input in Update so button presses aren't missed.
+#if CROSS_PLATFORM_INPUT
+        if (CrossPlatformInput.GetButtonDown("Jump")) jump = true;
+#else
+		if (Input.GetButtonDown("Jump")) jump = true;
+#endif
+
+    }
+
+	void FixedUpdate()
+	{
+		// Read the inputs.
+		bool crouch = Input.GetKey(KeyCode.LeftControl);
+		#if CROSS_PLATFORM_INPUT
+		float h = CrossPlatformInput.GetAxis("Horizontal");
+		#else
+		float h = Input.GetAxis("Horizontal");
+		#endif
+
+		// Pass all parameters to the character control script.
+		character.Move( h, crouch , jump );
+
+        // Reset the jump input once it has been used.
+	    jump = false;
+	}
+}
