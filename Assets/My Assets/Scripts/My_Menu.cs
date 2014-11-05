75b166f51e9d9f8f13f635498bcf036fdbc06294
Gabriel Tamassia Martinez <gabriel.tamassia.martinez@gmail.com>
2014-11-05 13:26:52 -0200

    Added menu

---
+++ Assets/My Assets/Scripts/My_Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class My_Menu : MonoBehaviour
+{
+	//public int score = 0;					// The player's score.
+	public string scene;
+
+	void Awake ()
+	{
+		// Setting up the reference.
+		//playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+	}
+
+
+	void Update ()
+	{
+		// Set the score text.
+		guiText.text = "Bibizin's Adventure:\n A volta dos que nao foram\n\n\n[Pressione qualquer tecla]";
+
+		if (Input.anyKey) 
+		{
+
+			Application.LoadLevel(scene);
+		}
+		// If the score has changed...
+		/*if (previousScore != score)
+			// ... play a taunt.
+			playerControl.StartCoroutine(playerControl.Taunt());
+
+		// Set the previous score to this frame's score.
+		previousScore = score;*/
+	}
+
+}
---
+++ Assets/My Assets/Scripts/My_Menu_Shadow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class My_Menu_Shadow : MonoBehaviour
+{
+	//public int score = 0;					// The player's score.
+	//public string scene;
+
+	void Awake ()
+	{
+		// Setting up the reference.
+		//playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+	}
+
+
+	void Update ()
+	{
+		// Set the score text.
+		this.guiText.text = GameObject.Find("Menu").GetComponent<GUIText>().text;
+	
+		// If the score has changed...
+		/*if (previousScore != score)
+			// ... play a taunt.
+			playerControl.StartCoroutine(playerControl.Taunt());
+
+		// Set the previous score to this frame's score.
+		previousScore = score;*/
+	}
+
+}
